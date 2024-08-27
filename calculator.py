78c5de433196ae11b6d6d4ae3038f744d45be490
Kishan Kumar Ganguly <kkganguly.iit.du@gmail.com>
2024-08-27 22:53:35 +0000

    modified addition to division

--- calculator.py
+++ calculator.py
@@ -1,10 +1,10 @@
-def add(x=1,y=1):
-    return x+y 
+def divide(x=1,y=1):
+    return x/y 
 def largest(lst):
     very_large = -11111111
     for num in lst:
         if num>very_large:
             very_large = num
     return very_large
-print(add())
+print(divide(5,2))
 print(largest(list(range(0,10))))
